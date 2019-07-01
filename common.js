34dec12a7e10ac6e766b0e448909a884d630c94b
Alex Koval <akoval@smartling.com>
2019-07-01 19:32:02 +0300

    Server accepts POST requests.

--- common.js
+++
@@ -1,9 +0,0 @@
-36: function(e, t, n) {
-        var i, r;
-        window.sl_notr_start(), i = [n(3)], r = function(e) {
-            "use strict";
-
-            s.prototype = {
-                toggle: function() {
-                    return this[this.isShown ? "hide" : "show"]()
-                }, window.sl_notr_end()
